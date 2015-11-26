f0b14134a0c1a092f14320592a743ceb1ef586dd
Marek Mystkowski <mm360351@orange08.mimuw.edu.pl>
2015-11-26 19:43:41 +0100

    ujednolicone wcięcia

--- very_long_int.cc
+++ very_long_int.cc
@@ -1,18 +1,4 @@
-/* Do zmiany jest : // XXXXXXXXXXXXXXXXXXXXXXXXXXXX
- * pozostałę funkcje/metody powinny pozostać bez zmian.
- */
-/* Założenia:
- * 1) przyjmujuemy też liczby ujemne do konstruktorów, wtedy żutujemy bitowo
- *    na liczby nieujemne.
- * 2) pilnujemy niezmienika że po każdej operacji:
- * 		-wektor data ma conajmniej jeden element.
- * 		-wektor data nie ma na końcu liczby == 0 (chyba że to jedyna wartość).
- */
-
-
 #include "very_long_int.h"
-#include <algorithm>
-#include <stack>
 using namespace std;
 
 static const uint64_t BASE = 1L << 32;
@@ -21,105 +7,110 @@ VeryLongInt::VeryLongInt(const VeryLongInt &x) : isNaN(x.isNaN), data(x.data) {
 }
 
 VeryLongInt::VeryLongInt(uint32_t x): isNaN(false) {
-	data.clear();
-	data.push_back(x);
+  data.clear();
+  data.push_back(x);
 }
 
 VeryLongInt::VeryLongInt(uint64_t x): isNaN(false){
-	data.clear();
-	uint32_t tempNumber;
-	tempNumber = (uint32_t) (x % (1LL << 32));
-	data.push_back(tempNumber);
-	tempNumber = (uint32_t) (x >> 32);
-	if(tempNumber > 0) data.push_back(tempNumber);
+  data.clear();
+  uint32_t tempNumber;
+  tempNumber = (uint32_t) (x % (1LL << 32));
+  data.push_back(tempNumber);
+  tempNumber = (uint32_t) (x >> 32);
+  if (tempNumber > 0) data.push_back(tempNumber);
 }
 
 VeryLongInt::VeryLongInt(const string &x){
-	VeryLongInt currentResult;
-	for(char c : x){
-		currentResult = (currentResult * 10) + (int)(c - '0');
-		if( c < '0' || c > '9')currentResult.isNaN = true;
-	}
-	*this = currentResult;
+  VeryLongInt currentResult;
+  for (char c : x){
+    currentResult = (currentResult * 10) + (int)(c - '0');
+    if ( c < '0' || c > '9') currentResult.isNaN = true;
+  }
+  *this = currentResult;
 }
 
 VeryLongInt::VeryLongInt(const char *x){
-	*this = VeryLongInt(string(x));
+  *this = VeryLongInt(string(x));
 }
+
 bool VeryLongInt::isValid()const{
-	return not isNaN;
+  return not isNaN;
 }
+
 void VeryLongInt::correct_invariants(){
-	if(data.size() == 0) data.push_back(0);
-	while(data[data.size() - 1] == 0 && data.size() > 1)
-		data.pop_back();
+  if (data.size() == 0) data.push_back(0);
+  while(data[data.size() - 1] == 0 && data.size() > 1)
+    data.pop_back();
 }
+
 VeryLongInt::operator bool() const{
-	if(not isValid()) return false;
-	if(*this == 0)return false;
-	return true;
+  if (not isValid()) return false;
+  if (*this == 0)return false;
+  return true;
 }
+
 size_t VeryLongInt::numberOfBinaryDigits()const{
-	if(not isValid())return 0;
-	if(*this == 0)return 1;
-	size_t result = (data.size() - 1) * 32;
-	uint32_t tmp = data[data.size() - 1];
-	while(tmp > 0){
-		tmp >>= 1;
-		result++;
-	}
-	cout << data.size() << endl;
-	return result;
+  if (not isValid())return 0;
+  if (*this == 0)return 1;
+  size_t result = (data.size() - 1) * 32;
+  uint32_t tmp = data[data.size() - 1];
+  while(tmp > 0){
+    tmp >>= 1;
+    result++;
+  }
+  return result;
 }
+
 VeryLongInt & VeryLongInt::operator=(const VeryLongInt &x){
-	isNaN = x.isNaN;
-	data = x.data;
-	return *this;
+  isNaN = x.isNaN;
+  data = x.data;
+  return *this;
 }
+
 VeryLongInt & VeryLongInt::operator+=(const VeryLongInt &x){
-	if(isNaN || x.isNaN)
-		isNaN = true;
-	else {
-		uint64_t tmp = 0L;
-		while(data.size() < x.data.size())
-			data.push_back(0);
-		data.push_back(0);
-		
-		for(size_t i = 0; i < x.data.size(); i++){
-			tmp += (uint64_t) x.data[i] + (uint64_t) data[i];
-			data[i] = (uint32_t) (tmp % BASE);
-			tmp /= BASE;
-		}
-		for(size_t i = x.data.size(); tmp > 0L; i++){
-			tmp += data[i];
-			data[i] = (uint32_t) (tmp % BASE);
-			tmp /= BASE;
-		}
-		correct_invariants();
-	}
-	return *this;
+  if (isNaN || x.isNaN)
+    isNaN = true;
+  else {
+    uint64_t tmp = 0L;
+    while(data.size() < x.data.size())
+      data.push_back(0);
+    data.push_back(0);
+    
+    for (size_t i = 0; i < x.data.size(); i++){
+      tmp += (uint64_t) x.data[i] + (uint64_t) data[i];
+      data[i] = (uint32_t) (tmp % BASE);
+      tmp /= BASE;
+    }
+    for (size_t i = x.data.size(); tmp > 0L; i++){
+      tmp += data[i];
+      data[i] = (uint32_t) (tmp % BASE);
+      tmp /= BASE;
+    }
+    correct_invariants();
+  }
+  return *this;
 }
 
 VeryLongInt & VeryLongInt::operator-=(const VeryLongInt &x){
-  if(isNaN || x.isNaN || data.size() < x.data.size())
+  if (isNaN || x.isNaN || data.size() < x.data.size())
     isNaN = true;
   else {
     uint64_t tmp = BASE;
-    for(size_t i = 0; i < x.data.size(); i++){
+    for (size_t i = 0; i < x.data.size(); i++){
       tmp +=  (uint64_t) data[i] - (uint64_t) x.data[i];
       data[i] = (uint32_t) (tmp % BASE);
-      if(tmp < BASE){
+      if (tmp < BASE){
         tmp = BASE - 1L;
       } else {
         tmp = BASE;
       }
     }
-    if(tmp < BASE){
-      if(data.size() > x.data.size()){
-		data[x.data.size()] -= 1;
-	  } else {
-	    isNaN = true;
-	  }
+    if (tmp < BASE){
+      if (data.size() > x.data.size()){
+        data[x.data.size()] -= 1;
+      } else {
+        isNaN = true;
+      }
     }
     correct_invariants();
   }
@@ -146,7 +137,7 @@ void VeryLongInt::multiplyByDigitAndShift(
 }
 
 VeryLongInt & VeryLongInt::operator*= (const VeryLongInt &x) {
-  if(isNaN || x.isNaN)
+  if (isNaN || x.isNaN)
     isNaN = true;
   else {
     VeryLongInt currentResult;
@@ -165,7 +156,7 @@ VeryLongInt & VeryLongInt::operator*= (const VeryLongInt &x) {
 }
 
 VeryLongInt & VeryLongInt::operator/=(const VeryLongInt &x){
-  if(isNaN || x.isNaN || x == 0)
+  if (isNaN || x.isNaN || x == 0)
     isNaN = true;
   else {
     const uint32_t lengthOfX = x.data.size();
@@ -211,44 +202,44 @@ VeryLongInt & VeryLongInt::operator/=(const VeryLongInt &x){
 }
 
 VeryLongInt & VeryLongInt::operator%=(const VeryLongInt &x){
-  if(isNaN || x.isNaN || x == 0){
-	  isNaN = true;
+  if (isNaN || x.isNaN || x == 0){
+    isNaN = true;
   } else {
-	VeryLongInt k = (*this / x);
-	*this -= k * x;
+  VeryLongInt k = (*this / x);
+  *this -= k * x;
   }
   return *this;
 }
 VeryLongInt & VeryLongInt::operator<<=(uint64_t x){
-  if(not isNaN){
+  if (not isNaN){
     uint64_t temp = x / 32;
-    if(temp > 0){
-		vector<uint32_t> helperVector = data;
-		size_t index = 0;
-		while(index < temp){
-			data.push_back(0);
-			data[index++] = 0;
-		}
-		for(uint32_t var : helperVector)
-			data[index++] = var;
-	}
-	temp = x - temp * 32;
-	
+    if (temp > 0){
+    vector<uint32_t> helperVector = data;
+    size_t index = 0;
+    while(index < temp){
+      data.push_back(0);
+      data[index++] = 0;
+    }
+    for (uint32_t var : helperVector)
+      data[index++] = var;
+  }
+  temp = x - temp * 32;
+  
     *this = *this * (1 << temp);
   }
   return *this;
 }
 VeryLongInt & VeryLongInt::operator>>=(uint64_t x){
-   if(not isNaN){
+   if (not isNaN){
     uint64_t temp = x / 32;
-    if(temp > 0){
-		for(size_t index= 0; index + temp  < data.size() ; index++)
-			data[index] = data[index + temp];
-		for(size_t index= 0; index < temp ; index++)
-			data.pop_back();
-	}
-	temp = x - temp * 32;
-	
+    if (temp > 0){
+    for (size_t index= 0; index + temp  < data.size() ; index++)
+      data[index] = data[index + temp];
+    for (size_t index= 0; index < temp ; index++)
+      data.pop_back();
+  }
+  temp = x - temp * 32;
+  
     *this = *this / (1 << temp);
   }
   return *this;
@@ -298,53 +289,53 @@ VeryLongInt &operator>>(const VeryLongInt &a, unsigned int b){
 }
 
 bool operator==(const VeryLongInt &a, const VeryLongInt &b){
-  if(not (a.isValid() && b.isValid()) ) return false;
+  if (not (a.isValid() && b.isValid()) ) return false;
   return ((a - b).isValid() && (b - a).isValid());
 }
 bool operator!=(const VeryLongInt &a, const VeryLongInt &b){
-  if(not (a.isValid() && b.isValid()) ) return false;
+  if (not (a.isValid() && b.isValid()) ) return false;
   return not (a == b);
 }
 bool operator<(const VeryLongInt &a, const VeryLongInt &b){
-  if(not (a.isValid() && b.isValid()) ) return false;
+  if (not (a.isValid() && b.isValid()) ) return false;
   return ((b - a).isValid() && not (a - b).isValid());
 }
 bool operator<=(const VeryLongInt &a, const VeryLongInt &b){
-  if(not (a.isValid() && b.isValid()) ) return false;
+  if (not (a.isValid() && b.isValid()) ) return false;
   return (b - a).isValid();
 }
 bool operator>(const VeryLongInt &a, const VeryLongInt &b){
-  if(not (a.isValid() && b.isValid()) ) return false;
+  if (not (a.isValid() && b.isValid()) ) return false;
   return not (a <= b);
 }
 bool operator>=(const VeryLongInt &a, const VeryLongInt &b){
-  if(not (a.isValid() && b.isValid()) ) return false;
+  if (not (a.isValid() && b.isValid()) ) return false;
   return not (a < b);
 }
 ostream & operator<<(ostream & os, const VeryLongInt & x){
-	if(not x.isValid()) os << "NaN";
-	else if(x <= 9) os << x.data[0];
-	else {
-		VeryLongInt temp = x;
-		stack<int> sta;
-		while(temp > 0){
-			sta.push((temp % 10).data[0]);
-			temp /= 10;
-		}
-		while(not sta.empty()){
-			os << sta.top();
-			sta.pop();
-		}
-	}
-	return os;
+  if (not x.isValid()) os << "NaN";
+  else if (x <= 9) os << x.data[0];
+  else {
+    VeryLongInt temp = x;
+    stack<int> sta;
+    while(temp > 0){
+      sta.push((temp % 10).data[0]);
+      temp /= 10;
+    }
+    while(not sta.empty()){
+      os << sta.top();
+      sta.pop();
+    }
+  }
+  return os;
 }
 
 VeryLongInt const &Zero(){
-	const static VeryLongInt global_zero = VeryLongInt();
-	return global_zero;
+  const static VeryLongInt global_zero = VeryLongInt();
+  return global_zero;
 }
 
 VeryLongInt const &NaN(){
-	const static VeryLongInt global_NaN = VeryLongInt(0) - VeryLongInt(1);
-	return global_NaN;
+  const static VeryLongInt global_NaN = VeryLongInt(0) - VeryLongInt(1);
+  return global_NaN;
 }
--- very_long_int.h
+++ very_long_int.h
@@ -3,98 +3,110 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <stack>
 
 class VeryLongInt{
-	private:
-		bool isNaN;
-		std::vector<uint32_t> data;
-		int date;
-		void correct_invariants();
-		void multiplyByDigitAndShift(VeryLongInt &, uint32_t, uint32_t) const;
-		
-	public:
-		VeryLongInt(const VeryLongInt &);
-		VeryLongInt(uint32_t arg = 0U);
-		VeryLongInt(uint64_t);
+  private:
+    bool isNaN;
+    std::vector<uint32_t> data;
+    int date;
+    void correct_invariants();
+    void multiplyByDigitAndShift(VeryLongInt &, uint32_t, uint32_t) const;
+    
+  public:
+    VeryLongInt(const VeryLongInt &);
+    VeryLongInt(uint32_t arg = 0U);
+    VeryLongInt(uint64_t);
+    VeryLongInt(int32_t x){
+      *this = VeryLongInt((uint32_t) x);
+    }
+    
+    VeryLongInt(int64_t x){
+      *this = VeryLongInt((uint64_t) x);
+    }
 
-		VeryLongInt(int32_t x){
-			*this = VeryLongInt((uint32_t) x);
-			}
-		VeryLongInt(int64_t x){
-			*this = VeryLongInt((uint64_t) x);
-			}
-
-		VeryLongInt(unsigned long long x){
-			*this = VeryLongInt((uint64_t) x);
-			}
-		VeryLongInt(long long x){
-			*this = VeryLongInt((uint64_t) x);
-			}
-		VeryLongInt(char) = delete;
-		VeryLongInt(char16_t) = delete;
-		VeryLongInt(char32_t) = delete;
-		VeryLongInt(wchar_t) = delete;
-		VeryLongInt(bool) = delete;
-		explicit VeryLongInt(const std::string &);
-		explicit VeryLongInt(const char *);
-		
-		explicit operator bool() const;
-		
-		
-		size_t numberOfBinaryDigits()const;
-		bool isValid()const;
-		
-		VeryLongInt &operator=(const VeryLongInt &);
-		VeryLongInt &operator+=(const VeryLongInt &);
-		VeryLongInt &operator-=(const VeryLongInt &);
-		VeryLongInt &operator*=(const VeryLongInt &);
-		VeryLongInt &operator/=(const VeryLongInt &);
-		VeryLongInt &operator%=(const VeryLongInt &);
-		VeryLongInt &operator<<=(uint64_t);
-		VeryLongInt &operator<<=(uint32_t x){
-			*this <<= (uint64_t) x;
-			return *this;
-		}
-		VeryLongInt &operator<<=(int32_t x){
-			*this <<= (uint64_t) x;
-			return *this;
-		}
-		VeryLongInt &operator<<=(int64_t x){
-			*this <<= (uint64_t) x;
-			return *this;
-		}
-		VeryLongInt &operator<<=(unsigned long long  x){
-			*this <<= (uint64_t) x;
-			return *this;
-		}
-		VeryLongInt &operator<<=(long long x){
-			*this <<= (uint64_t) x;
-			return *this;
-		}
-		VeryLongInt &operator>>=(uint64_t);
-		VeryLongInt &operator>>=(uint32_t x){
-			*this >>= (uint64_t) x;
-			return *this;
-		}
-		VeryLongInt &operator>>=(int32_t x){
-			*this >>= (uint64_t) x;
-			return *this;
-		}
-		VeryLongInt &operator>>=(int64_t x){
-			*this >>= (uint64_t) x;
-			return *this;
-		}
-		VeryLongInt &operator>>=(unsigned long long  x){
-			*this >>= (uint64_t) x;
-			return *this;
-		}
-		VeryLongInt &operator>>=(long long x){
-			*this >>= (uint64_t) x;
-			return *this;
-		}
-		
-		friend std::ostream & operator<<(std::ostream &, const VeryLongInt &);
-		
+    VeryLongInt(unsigned long long x){
+      *this = VeryLongInt((uint64_t) x);
+    }
+    
+    VeryLongInt(long long x){
+      *this = VeryLongInt((uint64_t) x);
+    }
+    
+    VeryLongInt(char) = delete;
+    VeryLongInt(char16_t) = delete;
+    VeryLongInt(char32_t) = delete;
+    VeryLongInt(wchar_t) = delete;
+    VeryLongInt(bool) = delete;
+    explicit VeryLongInt(const std::string &);
+    explicit VeryLongInt(const char *);
+    
+    explicit operator bool() const;
+    
+    size_t numberOfBinaryDigits()const;
+    bool isValid()const;
+    
+    VeryLongInt &operator=(const VeryLongInt &);
+    VeryLongInt &operator+=(const VeryLongInt &);
+    VeryLongInt &operator-=(const VeryLongInt &);
+    VeryLongInt &operator*=(const VeryLongInt &);
+    VeryLongInt &operator/=(const VeryLongInt &);
+    VeryLongInt &operator%=(const VeryLongInt &);
+    VeryLongInt &operator<<=(uint64_t);
+    VeryLongInt &operator<<=(uint32_t x){
+      *this <<= (uint64_t) x;
+      return *this;
+    }
+    
+    VeryLongInt &operator<<=(int32_t x){
+      *this <<= (uint64_t) x;
+      return *this;
+    }
+    
+    VeryLongInt &operator<<=(int64_t x){
+      *this <<= (uint64_t) x;
+      return *this;
+    }
+    
+    VeryLongInt &operator<<=(unsigned long long  x){
+      *this <<= (uint64_t) x;
+      return *this;
+    }
+    
+    VeryLongInt &operator<<=(long long x){
+      *this <<= (uint64_t) x;
+      return *this;
+    }
+    
+    VeryLongInt &operator>>=(uint64_t);
+    VeryLongInt &operator>>=(uint32_t x){
+      *this >>= (uint64_t) x;
+      return *this;
+    }
+    
+    VeryLongInt &operator>>=(int32_t x){
+      *this >>= (uint64_t) x;
+      return *this;
+    }
+    
+    VeryLongInt &operator>>=(int64_t x){
+      *this >>= (uint64_t) x;
+      return *this;
+    }
+    
+    VeryLongInt &operator>>=(unsigned long long  x){
+      *this >>= (uint64_t) x;
+      return *this;
+    }
+    
+    VeryLongInt &operator>>=(long long x){
+      *this >>= (uint64_t) x;
+      return *this;
+    }
+    
+    friend std::ostream & operator<<(std::ostream &, const VeryLongInt &);
+    
 };
 
 VeryLongInt &operator+(const VeryLongInt &, const VeryLongInt &);
